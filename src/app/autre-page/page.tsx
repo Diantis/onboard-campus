d1d3be24a1d6f2338773231003e0646035ef058c
Gab <diette.dev@gmail.com>
2025-04-08 22:43:47 +0200

    Fix: unescaped apostrophe in JSX

---
+++ src/app/autre-page/page.tsx
@@ -0,0 +1,7 @@
+export default function AutrePage() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold">Vous êtes sur l’autre page ✅</h1>
+    </main>
+  );
+}
