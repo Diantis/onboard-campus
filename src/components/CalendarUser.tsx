1eea0b280a42911609ffe7f7e10dd5b315cc2ffb
dorine <dorine.vigier@mediaschool.me>
2025-05-01 15:13:55 +0200

    feat: calendar integration

---
+++ src/components/CalendarUser.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  EventCalendar,
+  type CalendarEvent,
+} from "@/components/event-calendar";
+
+import {
+  addDays,
+  setHours,
+  setMinutes,
+} from "date-fns";
+
+const userEvents: CalendarEvent[] = [
+  {
+    id: "1",
+    title: "Product Launch",
+    description: "Big day!",
+    start: addDays(new Date(), 1),
+    end: addDays(new Date(), 2),
+    allDay: true,
+    color: "emerald",
+  },
+];
+
+export default function CalendarUser() {
+  return (
+    <EventCalendar
+      events={userEvents}
+      onEventAdd={() => {}}
+      onEventUpdate={() => {}}
+      onEventDelete={() => {}}
+      editable={false} // désactive drag, resize, clic, etc.
+    />
+  );
+}
