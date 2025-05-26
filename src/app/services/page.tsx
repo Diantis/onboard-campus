6fa53f14d7d965a5b0175a8221087a4d843631b8
Bjorn <bjorn.silvester@gmail.com>
2025-05-26 17:06:08 +0200

    feat: ajout des pages de services individuel

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,9 @@
+import Link from "next/link";
 import { IoIosSchool, IoMdRestaurant } from "react-icons/io";
 import { IoBook } from "react-icons/io5";
 import { FaHandshake } from "react-icons/fa";
 import { FaSchool } from "react-icons/fa6";
 import { BiHealth } from "react-icons/bi";
-
 import { Header } from "@/components/Header";
 import { BottomNav } from "@/components/BottomNav";
 import { FloatingMenu } from "@/components/FloatingMenu";
@@ -14,36 +14,42 @@ const serviceItems = [
     title: "Scolarité",
     IconComponent: FaSchool,
     color: "#00BCFF",
+    link: "/services/scolarite",
   },
   {
     id: "restauration",
     title: "Restauration",
     IconComponent: IoMdRestaurant,
     color: "#00D492",
+    link: "/services/restauration",
   },
   {
-    id: "vieAssociative",
+    id: "vie-associative",
     title: "Vie Associative",
     IconComponent: FaHandshake,
     color: "#FF637E",
+    link: "/services/vie-associative",
   },
   {
     id: "tutorat",
     title: "Tutorat",
     IconComponent: IoIosSchool,
     color: "#FDC700",
+    link: "/services/tutorat",
   },
   {
-    id: "aidesScolaires",
+    id: "aides-scolaires",
     title: "Aides Scolaires",
     IconComponent: IoBook,
     color: "#A684FF",
+    link: "/services/aides-scolaires",
   },
   {
     id: "sante",
     title: "Santé",
     IconComponent: BiHealth,
     color: "#FF8904",
+    link: "/services/sante",
   },
 ];
 
@@ -54,17 +60,18 @@ const Page = () => {
       <main className="flex-grow">
         <div className="w-full md:w-2/3 grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 p-4 sm:p-6 rounded-lg mx-auto my-8">
           {serviceItems.map((item) => {
-            const { id, title, IconComponent, color } = item;
+            const { id, title, IconComponent, color, link } = item;
 
             return (
-              <div
+              <Link
                 key={id}
+                href={link}
                 className="p-6 rounded-xl flex flex-col items-center justify-center text-center shadow-lg min-h-[180px] hover:scale-102 transition-transform duration-300 ease-in-out cursor-pointer"
                 style={{ backgroundColor: color }}
               >
                 <IconComponent size={68} className="text-white mb-3" />
                 <h2 className="text-white font-semibold text-lg">{title}</h2>
-              </div>
+              </Link>
             );
           })}
         </div>
