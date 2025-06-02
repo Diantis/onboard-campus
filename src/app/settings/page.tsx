2a88d876343e609caacb94b6d89b57cc98f4126f
Dorine <dorine.vigier@mediaschool.me>
2025-06-02 14:39:06 +0200

    refactor: settings title icon

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,15 +3,19 @@ import SettingsAccordion from "@/components/SettingsAccordion";
 import { Header } from "@/components/Header";
 import { BottomNav } from "@/components/BottomNav";
 import { FloatingMenu } from "@/components/FloatingMenu";
+import { Settings } from "lucide-react";
 
 export default function SettingsPage() {
   return (
     <div className="relative min-h-screen bg-background pb-28 flex flex-col">
-      <Header userName="Machin" />
-      <h1 className="md:mx-50 m-5 text-2xl font-bold mb-4">Paramètres ⚙️</h1>
-      <SettingsAccordion />
-      <FloatingMenu />
-      <BottomNav />
+		<Header userName="Machin" />
+		<h1 className="md:mx-50 m-5 text-2xl font-bold mb-4 flex items-center gap-2">
+		<Settings className="w-6 h-6" />
+		Paramètres
+		</h1>
+		<SettingsAccordion />
+		<FloatingMenu />
+		<BottomNav />
     </div>
   );
 }
