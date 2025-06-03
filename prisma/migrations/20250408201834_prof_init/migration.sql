b6d3a69405cc013bd0e6a42e24025031bb0e2413
Dorine <dorine.vigier@mediaschool.me>
2025-06-03 18:02:43 +0200

    refactor: change migration prisma

--- prisma/migrations/20250408201834_prof_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Prof" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Prof_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20250602194804_init/migration.sql
+++ prisma/migrations/20250602194804_init/migration.sql
@@ -3,9 +3,24 @@ CREATE TABLE "Student" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+    "address" TEXT,
+    "ine" TEXT,
+    "year" TEXT,
+    "course" TEXT,
+    "avatarUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Prof" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Prof_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
